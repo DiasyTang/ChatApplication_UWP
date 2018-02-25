e05f6bfe079b5b04a4c9616100d547080bc105fe
DESKTOP-OERRPPC\Tina <tina@niip.com>
2018-02-25 15:35:49 +0800

    Added Login Feature (Added LoginView and LoginViewModel)

---
+++ ChatUWPApplication/ViewModel/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatUWPApplication.ViewModel
+{
+    public class LoginViewModel:ViewModelBase
+    {
+        public RelayCommand<string> LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand<string>(LoginCommand_Execute, (n) => { return !string.IsNullOrWhiteSpace(n); });
+        }
+
+        private void LoginCommand_Execute(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ChatUWPApplication/ViewModel/ViewModelLocator.cs
+++ ChatUWPApplication/ViewModel/ViewModelLocator.cs
@@ -15,10 +15,11 @@ namespace ChatUWPApplication.ViewModel
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<LoginViewModel>();
         }
 
         public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
 
-       
+        public LoginViewModel Login => ServiceLocator.Current.GetInstance<LoginViewModel>();
     }
 }
